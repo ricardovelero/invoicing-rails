293416a2878e582031820572bb0f00ba1a8614f4
Ricardo Rodriguez <ricardovelero@gmail.com>
2022-09-25 21:40:10 +0200

    Limit title to 100 characters

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,6 @@
 class Item < ApplicationRecord
   validates :title, :price, presence: true
-  validates :title, uniqueness: true
+  validates :title, uniqueness: true, length: { maximum: 100 }
   validates :price, numericality: { greater_than_or_equal_to: 0.01 }
   validates :tax1, numericality: { greater_than_or_equal_to: 0.01 }
   validates :tax2, numericality: { greater_than_or_equal_to: 0.01 }
