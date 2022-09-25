f06841a597deaf45ba1d46e390cf278fd49a2bbe
Ricardo Rodriguez <ricardovelero@gmail.com>
2022-09-25 11:04:41 +0200

    Validation tests

--- test/models/item_test.rb
+++ test/models/item_test.rb
@@ -1,7 +1,19 @@
 require "test_helper"
 
 class ItemTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  fixtures :items
+  test "item price must be positive" do
+    item = Item.new(title: "Test Item", description: "This is a test")
+
+    item.price = -1
+    assert item.invalid?
+    assert_equal ["must be greater than or equal to 0.01"], item.errors[:price]
+
+    item.price = 0
+    assert item.invalid?
+    assert_equal ["must be greater than or equal to 0.01"], item.errors[:price]
+
+    item.price = 1
+    assert item.valid?
+  end
 end
