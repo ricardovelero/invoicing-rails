8dbc80f870d9b8e8eeb81acb8c70997b2554cab5
Ricardo Rodriguez <ricardovelero@gmail.com>
2022-09-30 21:50:57 +0200

    Model validation fix Form client_id in select box

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -1,9 +1,8 @@
 class Invoice < ApplicationRecord
-
   has_one :client
   belongs_to :user
 
   has_many :items, through: :line_items, dependent: :destroy
 
-  validates :client, presence: true
+  validates :client_id, presence: true
 end
