52cd677262182ce8649a2a86bd014eb3566409be
Ricardo Rodriguez <ricardovelero@gmail.com>
2022-10-03 17:19:54 +0200

    Fix associations

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -2,8 +2,9 @@ class Invoice < ApplicationRecord
   has_one :client
   belongs_to :user
 
-  has_many :line_items
-  has_many :items, through: :line_items, dependent: :destroy
+  has_many :line_items, dependent: :destroy
+  has_many :items, through: :line_items
+  accepts_nested_attributes_for :line_items, allow_destroy: true
 
   validates :client_id, presence: true
 
