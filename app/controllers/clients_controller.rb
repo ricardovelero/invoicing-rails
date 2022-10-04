0512eeee26ab57f98fa63cf84ad9ef25a6286c67
Ricardo Rodriguez <ricardovelero@gmail.com>
2022-10-04 21:30:55 +0200

    Add before_action :authenticate_user! But no to home

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -1,4 +1,5 @@
 class ClientsController < ApplicationController
+  before_action :authenticate_user!
   before_action :set_client, only: %i[ show edit update destroy ]
 
   # GET /clients or /clients.json
--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,6 +2,6 @@ class RegistrationsController < Devise::RegistrationsController
     protected
 
     def after_inactive_sign_up_path_for(resource)
-      '/terms' # Or :prefix_to_your_route
+      '/dashboard' # Or :prefix_to_your_route
     end
 end
