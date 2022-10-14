79cffa88c56e6db1115dc8cfe3fbe05288dd948b
Ricardo Rodriguez <ricardovelero@gmail.com>
2022-10-14 17:32:58 +0200

    Add panel navigation to profile

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,9 +1,10 @@
 import { Application } from "@hotwired/stimulus";
-import { Dropdown } from "tailwindcss-stimulus-components";
+import { Dropdown, Tabs } from "tailwindcss-stimulus-components";
 
 const application = Application.start();
 
 application.register("dropdown", Dropdown);
+application.register('tabs', Tabs);
 
 // Configure Stimulus development experience
 application.debug = false;
