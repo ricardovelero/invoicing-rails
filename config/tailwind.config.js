22155bdbaf6ee014f18d6150a6442ea3a6954dbb
Ricardo Rodriguez <ricardovelero@gmail.com>
2022-10-14 10:55:04 +0200

    Mozaic design with dropdown menu

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -5,23 +5,13 @@ module.exports = {
     './public/*.html',
     './app/helpers/**/*.rb',
     './app/javascript/**/*.js',
-    './app/views/**/*.{erb,haml,html,slim}'
+    './app/views/**/*.{erb,haml,html,slim}',
+    './vendor/javascript/tailwindcss-stimulus-components.js'
   ],
   theme: {
     extend: {
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
-      },
-      spacing: {
-        '72': '18rem',
-        '84': '21rem',
-        '96': '24rem',
-      },
-      colors: {
-        'main': {
-          DEFAULT: '#60b0e2',
-          '500': '#41a1dd',
-        },
       },
     },
   },
