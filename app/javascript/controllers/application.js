03203b2b16adff078f20fd2f7d505ecc60c81e0d
Ricardo Rodriguez <ricardovelero@gmail.com>
2022-10-14 10:55:04 +0200

    Mozaic design with dropdown menu

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,6 +1,9 @@
 import { Application } from "@hotwired/stimulus"
+import { Dropdown } from "tailwindcss-stimulus-components"
 
 const application = Application.start()
+
+application.register('dropdown', Dropdown);
 
 // Configure Stimulus development experience
 application.debug = false
