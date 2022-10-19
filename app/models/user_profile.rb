0b596b296bf244faca75380a0214fc04c6393767
Ricardo Rodriguez <ricardovelero@gmail.com>
2022-10-19 21:38:17 +0200

    Def full_name

--- app/models/user_profile.rb
+++ app/models/user_profile.rb
@@ -1,3 +1,7 @@
 class UserProfile < ApplicationRecord
   belongs_to :user, inverse_of: :user_profile
+
+  def full_name
+    [first_name, last_name].reject(&:blank?).collect(&:capitalize).join(" ")
+  end
 end
