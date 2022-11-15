2adb037c696e036ae294e24945df4a9bb502d480
Ricardo Rodriguez <ricardovelero@gmail.com>
2022-11-15 11:03:17 +0100

    Add gravatar to navbar

--- app/helpers/user_profile_helper.rb
+++ app/helpers/user_profile_helper.rb
@@ -5,4 +5,10 @@ module UserProfileHelper
     gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
     image_tag(gravatar_url, alt: user.user_profile.full_name, class: "gravatar", width: "80", height: "80")
   end
+
+  def gravatar_small_for(user)
+    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
+    image_tag(gravatar_url, alt: user.user_profile.full_name, class: "w-8 h-8 rounded-full", width: "32", height: "32")
+  end
 end
