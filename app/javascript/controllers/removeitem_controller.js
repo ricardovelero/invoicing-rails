a7f334e7fb49a585e18ea3f2f49c7607daa49ce0
Ricardo Rodriguez <ricardovelero@gmail.com>
2022-11-16 22:53:07 +0100

    Add remove item with stimulus

---
+++ app/javascript/controllers/removeitem_controller.js
@@ -0,0 +1,14 @@
+import { Controller } from "@hotwired/stimulus";
+
+export default class extends Controller {
+  static targets = ["item"];
+
+  click(event) {
+    let eventId = event.path[3].id
+    this.itemTargets.forEach((element) => {
+      if (element.id == eventId) {
+        element.remove()
+      }
+    })
+  }
+}
