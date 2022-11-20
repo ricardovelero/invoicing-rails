5f6d8e98ca2060d10e1d9e28f536f38d6d6a0a8f
Ricardo Rodriguez <ricardovelero@gmail.com>
2022-11-20 20:36:49 +0100

    Fix for empty quantity

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -2,21 +2,29 @@ class LineItem < ApplicationRecord
   belongs_to :invoice
   belongs_to :item
 
+  validates :quantity, presence: true
+
   def sub_total
-    if item
+    if item && quantity
       quantity * item.price
+    else
+      return 0
     end
   end
 
   def total_price
-    if item
+    if item && quantity
       sub_total * (1 + item.iva / 100)
+    else
+      return 0
     end
   end
 
   def total_iva
-    if item
+    if item && quantity
       total_price - sub_total
+    else
+      return 0
     end
   end
 end
