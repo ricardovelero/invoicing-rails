1e6b3079f8793fb2cd416251e4c1283de692d3cc
Ricardo Rodriguez <ricardovelero@gmail.com>
2022-11-28 11:34:10 +0100

    Add email to user_profile

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_19_090253) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_26_124039) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -88,6 +88,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_19_090253) do
     t.string "company_name"
     t.string "first_name"
     t.string "last_name"
+    t.string "email"
     t.index ["user_id"], name: "index_user_profiles_on_user_id"
   end
 
