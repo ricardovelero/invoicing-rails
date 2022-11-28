6f0acb8f67eb2912edbd66537144afc88883f56e
Ricardo Rodriguez <ricardovelero@gmail.com>
2022-11-28 11:34:30 +0100

    Add address methods

--- app/models/user_profile.rb
+++ app/models/user_profile.rb
@@ -4,4 +4,20 @@ class UserProfile < ApplicationRecord
   def full_name
     [first_name, last_name].reject(&:blank?).collect(&:capitalize).join(" ")
   end
+
+  def address
+    [street_address_1, street_address_2, city, postal_code, region, country].reject(&:blank?).collect(&:titleize).join(", ")
+  end
+
+  def address_line_1
+    [street_address_1].reject(&:blank?).collect(&:titleize).join(", ")
+  end
+
+  def address_line_2
+    [street_address_2].reject(&:blank?).collect(&:titleize).join(", ")
+  end
+
+  def address_line_3
+    [city, postal_code, region, country].reject(&:blank?).collect(&:titleize).join(", ")
+  end
 end
