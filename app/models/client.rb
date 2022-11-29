a79eb706f17f4269c44f91d9615e0b505b473fc5
Ricardo Rodriguez <ricardovelero@gmail.com>
2022-11-29 12:01:59 +0100

    Add PG search scope

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,4 +1,7 @@
 class Client < ApplicationRecord
+  include PgSearch::Model
+  pg_search_scope :search, against: [:first_name, :last_name, :nif, :city, :region, :country],
+    using: { tsearch: { prefix: true } }
   belongs_to :user
   has_many :invoices
 
