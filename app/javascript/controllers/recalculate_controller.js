bb9c9000c1ebb4694f9efe09f81580806130a12c
Ricardo Rodriguez <ricardovelero@gmail.com>
2022-12-01 15:36:20 +0100

    Refactor calculations and fix small bugs

--- app/javascript/controllers/recalculate_controller.js
+++ app/javascript/controllers/recalculate_controller.js
@@ -1,23 +1,7 @@
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
-  static targets = ["total", "price", "tax", "subtotal", "taxtotal", "grandtotal"];
-
-  initialize() {
-    // let theids = []
-    // this.theidTargets.forEach((element, index) => {
-    //   theids[index] = element.id.match(/\d/g).join("")
-    // })
-    // this.totalTargets.forEach((element, index) => {
-    //   element.id = theids[index]
-    // })
-    // this.priceTargets.forEach((element, index) => {
-    //   element.id = theids[index]
-    // })
-    // this.taxTargets.forEach((element, index) => {
-    //   element.id = theids[index]
-    // })
-  }
+  static targets = ["qty", "price", "tax", "total", "subtotal", "taxtotal", "grandtotal"];
 
   recalculate(event) {
     const formatter = new Intl.NumberFormat(undefined, {
@@ -25,41 +9,39 @@ export default class extends Controller {
       currency: 'EUR',
     });
 
-    let eventId = event.target.id.match(/\d/g).join("")
-    let itemPrice = ""
-    let itemTax = ""
-    let subTotal = 0
     let itemTotal = 0
-    let sum = 0
+    let subTotal = 0
+    let grandTotal = 0
     let taxesArray = []
+    let qtysArray = []
+    let pricesArray = []
 
     this.taxTargets.forEach((tax, index) => {
-      taxesArray[index] = tax.textContent
+      taxesArray[index] = tax.textContent.trim() * 1
+    })
+
+    this.qtyTargets.forEach((qty, index) => {
+      qtysArray[index] = qty.value
+    })
+  
+    this.priceTargets.forEach((price, index) => {
+      pricesArray[index] = this.convertNum(price.textContent)
     })
 
     this.totalTargets.forEach((element, index) => {
-      if (element.id.match(/\d/g).join("") == eventId) {
-        this.priceTargets.forEach((price, index) => {
-          if (price.id.match(/\d/g).join("") == eventId) {
-            itemPrice = this.convertNum(price.textContent)
-          }
-        })
-        this.taxTargets.forEach((tax, index) => {
-          if (tax.id.match(/\d/g).join("") == eventId) {
-            itemTax = tax.textContent
-          }
-        })
-        itemTotal = itemPrice * (1 + itemTax/100) * event.target.value
-        element.textContent = formatter.format(itemTotal)
-      }
-      sum += this.convertNum(element.textContent)
+
+      itemTotal = pricesArray[index] * (1 + taxesArray[index]/100) * qtysArray[index]
+      
+      element.textContent = formatter.format(itemTotal)
+
+      grandTotal += itemTotal
  
-      subTotal += this.convertNum(element.textContent) / (1 + taxesArray[index]/100) 
+      subTotal += itemTotal / (1 + taxesArray[index]/100) 
     })
 
     this.subtotalTarget.textContent = formatter.format(subTotal)
-    this.taxtotalTarget.textContent = formatter.format(sum - subTotal)
-    this.grandtotalTarget.textContent = formatter.format(sum)
+    this.taxtotalTarget.textContent = formatter.format(grandTotal - subTotal)
+    this.grandtotalTarget.textContent = formatter.format(grandTotal)
   }
 
   convertNum(element) {
