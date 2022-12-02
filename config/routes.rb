fbb951671f0e16684ac4f504059b737e5d1a4c8a
Ricardo Rodriguez <ricardovelero@gmail.com>
2022-12-02 15:47:01 +0100

    Now root is Dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root "home#index"
+  root "dashboard#index"
   devise_for :users, controllers: { registrations: "registrations" }
   resources :users
   resources :clients
