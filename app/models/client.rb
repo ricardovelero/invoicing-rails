3ceda3e07c0a1b8c32125a7b963736dc829984e9
Ricardo Rodriguez <ricardovelero@gmail.com>
2022-12-08 11:49:03 +0100

    Add country and regions, needs more testing

--- app/models/client.rb
+++ app/models/client.rb
@@ -36,4 +36,28 @@ class Client < ApplicationRecord
     return Country.named(country)&.alpha_2_code if country.count("a-zA-Z") > 2
     country
   end
+
+  def get_regions
+    @regions = Array.new
+    if new_record?
+      Country.alpha_2_coded("ES").subregions.each { |r| @regions << r.subregions }
+    else
+      if country.count("a-zA-Z") > 2
+        Country.named(country)&.subregions&.each { |r| @regions << r.subregions }
+        @regions = @regions.reject { |c| c.empty? }
+        if @regions.empty?
+          @regions = Array.new
+          Country.named(country)&.subregions&.each { |r| @regions << r }
+        end
+      else
+        Country.alpha_2_coded(country).subregions.each { |r| @regions << r.subregions }
+        @regions = @regions.reject { |c| c.empty? }
+        if @regions.empty?
+          @regions = Array.new
+          Country.alpha_2_coded(country).subregions.each { |r| @regions << r }
+        end
+      end
+    end
+    @regions.flatten.sort
+  end
 end
