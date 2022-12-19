3b4a01f1297f643393e45f4b2595f152539d6e00
Ricardo Rodriguez <ricardovelero@gmail.com>
2022-12-19 15:00:42 +0100

    Add css and stimulus controller

--- app/javascript/controllers/flatpickr_controller.js
+++ app/javascript/controllers/flatpickr_controller.js
@@ -1,7 +1,12 @@
-import { Controller } from "@hotwired/stimulus"
+import Flatpickr from 'stimulus-flatpickr'
 
-// Connects to data-controller="flatpickr"
-export default class extends Controller {
+export default class extends Flatpickr {
+  static targets = ["start", "end"]
+
   connect() {
+    flatpickr(this.startTarget)
+    flatpickr(this.endTarget)
   }
+
+  disconnect() {}
 }
