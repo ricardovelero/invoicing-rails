891d678611b6e478317b8ffbe9a8a2e822e04b4c
Ricardo Rodriguez <ricardovelero@gmail.com>
2022-12-19 19:05:18 +0100

    Fix alpinejs loading with importmap

--- config/importmap.rb
+++ config/importmap.rb
@@ -7,3 +7,5 @@ pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "tailwindcss-stimulus-components" # @3.0.4
 pin "apexcharts", to: "https://ga.jspm.io/npm:apexcharts@3.36.3/dist/apexcharts.common.js"
+pin "alpinejs", to: "https://ga.jspm.io/npm:alpinejs@3.10.5/dist/module.esm.js"
+pin "alpine-turbo-drive-adapter", to: "https://ga.jspm.io/npm:alpine-turbo-drive-adapter@2.0.0/dist/alpine-turbo-drive-adapter.esm.js"
