3253fca50aad725849fce19cdf8e0c1948bd10b6
Ricardo Rodriguez <ricardovelero@gmail.com>
2022-12-19 13:45:14 +0100

    Add apexcharts Add invoices chart

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,5 +1,9 @@
 class DashboardController < ApplicationController
   before_action :authenticate_user!
   def index
+    report_data = Charts::InvoicesChart.new(current_user).generate
+
+    @categories = report_data.keys.to_json
+    @series = report_data.values.to_json
   end
 end
