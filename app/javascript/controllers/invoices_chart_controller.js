484b99378507943d376b61e2353d3702ac1c300a
Ricardo Rodriguez <ricardovelero@gmail.com>
2022-12-20 13:33:37 +0100

    Refactor chart code and clean up

--- app/javascript/controllers/invoices_chart_controller.js
+++ app/javascript/controllers/invoices_chart_controller.js
@@ -1,19 +1,7 @@
-import { Controller } from "@hotwired/stimulus"
-import ApexCharts from "apexcharts"
+import ChartsController from './charts_controller'
 
-// Connects to data-controller="invoices-chart"
-export default class extends Controller {
-  static targets = ["chart"]
+export default class extends ChartsController {
 
-  static values = {
-    categories: Array,
-    series: Array
-  }
-
-  initialize() {
-    this.chart = new ApexCharts(this.chartTarget, this.chartOptions);
-    this.chart.render();
-  }
   get chartOptions() {
     return {
       chart: {
@@ -21,11 +9,11 @@ export default class extends Controller {
         type: 'line',
       },
       series: [{
-        name: 'Invoices',
+        name: 'Applicants',
         data: this.seriesValue
       }],
       xaxis: {
-        categories: this.categoriesValue,
+        categories: this.labelsValue,
         type: 'datetime'
       },
       stroke: {
