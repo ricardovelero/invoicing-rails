d2f29245aad1c831207bb9b8291de60ec6665816
Ricardo Rodriguez <ricardovelero@gmail.com>
2022-12-20 21:08:27 +0100

    Add translations

--- app/helpers/dashboard_helper.rb
+++ app/helpers/dashboard_helper.rb
@@ -2,11 +2,11 @@ module DashboardHelper
 
   def greet
     case Time.zone.now.hour
-    when 4..11 then 'Buenos días'
-    when 12..19 then 'Buenas tardes'
-    when 20..23 then 'Buenas noches'
+    when 4..11 then I18n.t('buenos_dias')
+    when 12..19 then I18n.t('buenas_tardes')
+    when 20..23 then I18n.t('buenas_noches')
     else
-      '¡Hola!'
+      I18n.t('hola')
     end
   end
 
