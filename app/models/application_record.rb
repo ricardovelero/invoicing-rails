0bfea5bb17db9c416ed2275c1eb4bf6270710a03
Ricardo Rodriguez <ricardovelero@gmail.com>
2023-04-04 14:09:30 +0200

    Move country_code & get_regions methods

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,4 +1,34 @@
 class ApplicationRecord < ActiveRecord::Base
   include PgSearch::Model
   primary_abstract_class
+
+  def get_regions
+    @regions = Array.new
+    if new_record?
+      Country.alpha_2_coded("ES").subregions.each { |r| @regions << r.subregions }
+    else
+      if country.count("a-zA-Z") > 2
+        Country.named(country)&.subregions&.each { |r| @regions << r.subregions }
+        @regions = @regions.reject { |c| c.empty? }
+        if @regions.empty?
+          @regions = Array.new
+          Country.named(country)&.subregions&.each { |r| @regions << r }
+        end
+      else
+        Country.alpha_2_coded(country).subregions.each { |r| @regions << r.subregions }
+        @regions = @regions.reject { |c| c.empty? }
+        if @regions.empty?
+          @regions = Array.new
+          Country.alpha_2_coded(country).subregions.each { |r| @regions << r }
+        end
+      end
+    end
+    @regions.flatten.sort
+  end
+  
+  def country_code
+    return "ES" if country.nil?
+    return Country.named(country)&.alpha_2_code if country.count("a-zA-Z") > 2
+    country
+  end
 end
--- app/models/client.rb
+++ app/models/client.rb
@@ -32,34 +32,4 @@ class Client < ApplicationRecord
   def address_line_2
     [city, postal_code, region, country].reject(&:blank?).collect(&:titleize).join(", ")
   end
-
-  def country_code
-    return "ES" if country.nil?
-    return Country.named(country)&.alpha_2_code if country.count("a-zA-Z") > 2
-    country
-  end
-
-  def get_regions
-    @regions = Array.new
-    if new_record?
-      Country.alpha_2_coded("ES").subregions.each { |r| @regions << r.subregions }
-    else
-      if country.count("a-zA-Z") > 2
-        Country.named(country)&.subregions&.each { |r| @regions << r.subregions }
-        @regions = @regions.reject { |c| c.empty? }
-        if @regions.empty?
-          @regions = Array.new
-          Country.named(country)&.subregions&.each { |r| @regions << r }
-        end
-      else
-        Country.alpha_2_coded(country).subregions.each { |r| @regions << r.subregions }
-        @regions = @regions.reject { |c| c.empty? }
-        if @regions.empty?
-          @regions = Array.new
-          Country.alpha_2_coded(country).subregions.each { |r| @regions << r }
-        end
-      end
-    end
-    @regions.flatten.sort
-  end
 end
