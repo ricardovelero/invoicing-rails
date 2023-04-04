a3e66f272a7ea404d179bdfdcdf176571105c134
Ricardo Rodriguez <ricardovelero@gmail.com>
2023-04-04 12:26:49 +0200

    Fix event.path deprecated Add polyfill for older browsers

--- app/javascript/controllers/removeitem_controller.js
+++ app/javascript/controllers/removeitem_controller.js
@@ -1,27 +1,52 @@
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
-  static targets = ["item", "theHidden", "qty"];
+    static targets = ["item", "theHidden", "qty"];
 
-  click(event) {
-    let eventId = event.path[3].id
-    this.itemTargets.forEach((element) => {
-      if (element.id == eventId) {
-        element.classList.add('transform', 'opacity-0', 'transition', 'duration-1000');
-        setTimeout(() => element.classList.add('hidden'), 800)
-      }
-    })
-    this.theHiddenTargets.forEach((element) => {
-      let hiddenId = element.id.match(/\d+/g)
-      if (hiddenId == eventId) {
-        element.value = true
-      }
-    })
-    this.qtyTargets.forEach((element) => {
-      let qtyId = element.id.match(/\d+/g)
-      if (qtyId == eventId) {
-        element.value = 0
-      }
-    })
-  }
+    click(event) {
+        let path = event.path || (event.composedPath && event.composedPath());
+        let eventId = path[3].id;
+        this.itemTargets.forEach((element) => {
+            if (element.id == eventId) {
+                element.classList.add(
+                    "transform",
+                    "opacity-0",
+                    "transition",
+                    "duration-1000"
+                );
+                setTimeout(() => element.classList.add("hidden"), 800);
+            }
+        });
+        this.theHiddenTargets.forEach((element) => {
+            let hiddenId = element.id.match(/\d+/g);
+            if (hiddenId == eventId) {
+                element.value = true;
+            }
+        });
+        this.qtyTargets.forEach((element) => {
+            let qtyId = element.id.match(/\d+/g);
+            if (qtyId == eventId) {
+                element.value = 0;
+            }
+        });
+    }
 }
+// Event.composedPath
+(function (e, d, w) {
+    if (!e.composedPath) {
+        e.composedPath = function () {
+            if (this.path) {
+                return this.path;
+            }
+            var target = this.target;
+
+            this.path = [];
+            while (target.parentNode !== null) {
+                this.path.push(target);
+                target = target.parentNode;
+            }
+            this.path.push(d, w);
+            return this.path;
+        };
+    }
+})(Event.prototype, document, window);
