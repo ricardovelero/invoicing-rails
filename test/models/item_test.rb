f45f44293fa7db0d4d6120257baa4ac29d57c145
Ricardo Rodriguez <ricardovelero@gmail.com>
2023-04-07 22:17:57 +0200

    Add model testing

--- test/models/item_test.rb
+++ test/models/item_test.rb
@@ -1,39 +1,62 @@
 require "test_helper"
 
 class ItemTest < ActiveSupport::TestCase
-  fixtures :items
+  # fixtures :items
+  test "should not save item without name" do
+    item = Item.new
+    assert_not item.save, "Saved the article without a title"
+  end
+
   test "item attributes must not be empty" do
     item = Item.new
     assert item.invalid?
-    assert item.errors[:title].any?
+    assert item.errors[:item_name].any?
     assert item.errors[:price].any?
   end
 
   test "item price must be positive" do
-    item = Item.new(title: "Test Item", description: "This is a test")
-
+    item = Item.new(item_name: "Price Test", description: "Test for negative price", iva:10)
     item.price = -1
     assert item.invalid?
-    assert_equal ["must be greater than or equal to 0.01"], item.errors[:price]
+    assert item.errors[:price].any?
 
+    item = Item.new(item_name: "Price Test", description: "Test for zero price", iva:21)
     item.price = 0
     assert item.invalid?
-    assert_equal ["must be greater than or equal to 0.01"], item.errors[:price]
+    assert_not item.errors[:price].any?
 
+    item = Item.new(item_name: "Price Test", description: "Test for positive price", iva:21)
     item.price = 1
-    assert item.valid?
-  end
+    assert item.invalid?
+    assert_not item.errors[:price].any?
 
-  test "item is not valid without a unique title" do
+  end
+  test "iva must be positive" do
+    item = Item.new(item_name: "IVA Test", description: "Test for negative iva", price:10)
+    item.iva = -1
+    assert item.invalid?
+    assert item.errors[:iva].any?
+
+    item = Item.new(item_name: "IVA Test", description: "Test for zero iva", price:21)
+    item.iva = 0
+    assert item.invalid?
+    assert_not item.errors[:iva].any?
+
+    item = Item.new(item_name: "IVA Test", description: "Test for positive iva", price:21)
+    item.iva = 1
+    assert item.invalid?
+    assert_not item.errors[:iva].any?
+
+  end  
+  test "item is not valid without a unique name" do
     item =
       Item.new(
-        title: items(:item).title,
+        item_name: items(:first).item_name,
         description: "yyy",
         price: 1,
-        iva: 21,
-        irpf: 4
+        iva: 21
       )
     assert item.invalid?
-    assert_equal [I18n.translate("errors.messages.taken")], item.errors[:title]
+    assert_equal [I18n.translate("errors.messages.taken")], item.errors[:item_name]
   end
 end
