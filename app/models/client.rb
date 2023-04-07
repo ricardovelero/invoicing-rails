9a6ea0fae62949e222331f083f9ad604d10fb82d
Ricardo Rodriguez <ricardovelero@gmail.com>
2023-04-07 21:57:46 +0200

    Add some field length validations

--- app/models/client.rb
+++ app/models/client.rb
@@ -11,6 +11,10 @@ class Client < ApplicationRecord
             :postal_code,
             :country,
             presence: true
+  
+  validates :street, length: { maximum: 70 }
+  validates :first_name, :last_name, :city, :region, :country, length: { maximum: 50 }
+  validates :nif, :postal_code, length: { maximum: 12 }
     
   pg_search_scope :search, 
     against: [:first_name, :last_name, :nif, :city, :region, :country],
