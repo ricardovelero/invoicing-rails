bae8c78c8210079f26bf61e3d7c650459a047866
Ricardo Rodriguez <ricardovelero@gmail.com>
2024-02-12 10:36:36 +0100

    Refactor, document, and add turbo_stream response

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -1,3 +1,9 @@
+# frozen_string_literal: true
+
+# ClientsController handles CRUD operations for clients in the application.
+# It requires authentication for all actions and ensures that each action
+# operates on the client associated with the current user.
+#
 class ClientsController < ApplicationController
   before_action :authenticate_user!
   before_action :set_client, only: %i[show edit update destroy]
@@ -5,16 +11,16 @@ class ClientsController < ApplicationController
   # GET /clients or /clients.json
   def index
     @clients = current_user.clients.includes(:invoices)
-    @clients = current_user.clients.includes(:invoices).search(params[:query]) if params[:query].present?
-    @pagy, @clients = pagy @clients.reorder(sort_column => sort_direction), items: params.fetch(:count, 10)
+    apply_search_query if params[:query].present?
+    apply_pagination
   end
 
   def sort_column
-    %w{ first_name email nif }.include?(params[:sort]) ? params[:sort] : "first_name"
+    %w[first_name email nif].include?(params[:sort]) ? params[:sort] : 'first_name'
   end
 
   def sort_direction
-    %w{ asc desc }.include?(params[:direction]) ? params[:direction] : "asc"
+    %w[asc desc].include?(params[:direction]) ? params[:direction] : 'asc'
   end
 
   # GET /clients/1 or /clients/1.json
@@ -31,46 +37,18 @@ class ClientsController < ApplicationController
   end
 
   # GET /clients/1/edit
-  def edit
-  end
+  def edit; end
 
   # POST /clients or /clients.json
   def create
-    @client = Client.new(client_params)
+    @client = build_client
     @client.user = current_user
-
-    respond_to do |format|
-      if @client.save
-        format.html do
-          redirect_to clients_url,
-                      notice: "Client was successfully created."
-        end
-        format.json { render :show, status: :created, location: @client }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json do
-          render json: @client.errors, status: :unprocessable_entity
-        end
-      end
-    end
+    respond_to { |format| handle_response(format) }
   end
 
   # PATCH/PUT /clients/1 or /clients/1.json
   def update
-    respond_to do |format|
-      if @client.update(client_params)
-        format.html do
-          redirect_to clients_url,
-                      notice: "Client was successfully updated."
-        end
-        format.json { render :show, status: :ok, location: @client }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json do
-          render json: @client.errors, status: :unprocessable_entity
-        end
-      end
-    end
+    respond_to { |format| handle_update_response(format) }
   end
 
   # DELETE /clients/1 or /clients/1.json
@@ -79,7 +57,7 @@ class ClientsController < ApplicationController
 
     respond_to do |format|
       format.html do
-        redirect_to clients_url, notice: "Client was successfully destroyed."
+        redirect_to clients_url, notice: I18n.t('client_destroyed')
       end
       format.json { head :no_content }
     end
@@ -92,8 +70,41 @@ class ClientsController < ApplicationController
     @client = Client.find(params[:id])
   end
 
+  def apply_search_query
+    @clients = @clients.search(params[:query])
+  end
+
+  def apply_pagination
+    @pagy, @clients = pagy(@clients.reorder(sort_column => sort_direction), items: params.fetch(:count, 10))
+  end
+
+  def build_client
+    Client.new(client_params)
+  end
+
+  def handle_response(format)
+    if @client.save
+      format.html { redirect_to clients_url, notice: I18n.t('client_created') }
+      format.json { render :show, status: :created, location: @client }
+      format.turbo_stream
+    else
+      format.html { render :new, status: :unprocessable_entity }
+      format.json { render json: @client.errors, status: :unprocessable_entity }
+    end
+  end
+
+  def handle_update_response(format)
+    if @client.update(client_params)
+      format.html { redirect_to clients_url, notice: I18n.t('client_updated') }
+      format.json { render :show, status: :ok, location: @client }
+    else
+      format.html { render :edit, status: :unprocessable_entity }
+      format.json { render json: @client.errors, status: :unprocessable_entity }
+    end
+  end
+
   # Only allow a list of trusted parameters through.
-  def client_params
+  def client_params # rubocop:disable Metrics/MethodLength
     params.require(:client).permit(
       :user_id,
       :first_name,
