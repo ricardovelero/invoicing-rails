17333ab51cb35cfe4b1bef5d2e88d1a4dc2bfd7f
Ricardo Rodriguez <ricardovelero@gmail.com>
2024-02-12 17:00:39 +0100

    Add close con clik outside modal with alpine

--- app/javascript/controllers/modal_controller.js
+++ app/javascript/controllers/modal_controller.js
@@ -5,16 +5,16 @@ export default class extends Controller {
   static targets = ["modal"];
 
   connect() {
-    console.log("Modal stimulus loaded...");
+    window.addEventListener("invokeStimulusClose", (event) => {
+      this.close(event);
+    });
   }
 
   open() {
-    console.log("Open");
     this.modalTarget.classList.remove("opacity-0", "pointer-events-none");
   }
 
   close(event) {
-    console.log("Close");
     event.stopPropagation();
     this.modalTarget.classList.add("opacity-0", "pointer-events-none");
   }
