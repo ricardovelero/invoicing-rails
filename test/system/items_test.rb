097d60cd69f7e7c2b6fa1cacb34376d6c2d32600
Ricardo Rodriguez <ricardovelero@gmail.com>
2024-03-26 16:07:29 +0100

    Update testing

--- test/system/items_test.rb
+++ test/system/items_test.rb
@@ -1,53 +1,58 @@
-require "application_system_test_case"
+# frozen_string_literal: true
+
+require 'application_system_test_case'
 
 class ItemsTest < ApplicationSystemTestCase
   setup do
+    login_as users(:third)
     @item = items(:first) # Reference to the first fixture item
   end
 
-  test "Showing an item" do
+  test 'Showing an item' do
     visit items_path
     click_link @item.name
 
-    assert_selector "h1", text: @item.name
+    assert_selector 'h1', text: @item.name
   end
 
-  test "should create item" do
+  test 'Should create item' do
     visit items_path
-    click_on "Nuevo ítem"
+    click_on 'Nuevo Ítem'
 
-    fill_in "Description", with: @item.description
-    fill_in "Price", with: @item.price
-    fill_in "IVA", with: @item.iva
-    fill_in "IRPF", with: @item.irpf
-    fill_in "Item name", with: @item.item_name
-    click_on "Crear Item"
+    fill_in 'Description', with: @item.description
+    fill_in 'Price', with: @item.price
+    fill_in 'IVA', with: @item.iva
+    fill_in 'IRPF', with: @item.irpf
+    fill_in 'Item name', with: @item.item_name
+    click_on 'Crear Item'
 
-    assert_text "Item was successfully created"
-    click_on "Back"
+    assert_text 'Item was successfully created'
+    click_on 'Back'
   end
 
-  test "should update Item" do
+  test 'should update Item' do
     visit items_path
-    click_on "Edit this item", match: :first
+    click_on 'Edit this item', match: :first
 
-    fill_in "Description", with: @item.description
-    fill_in "Price", with: @item.price
-    fill_in "IVA", with: @item.iva
-    fill_in "IRPF", with: @item.irpf
-    fill_in "Item name", with: @item.item_name
-    click_on "Update Item"
+    fill_in 'Description', with: @item.description
+    fill_in 'Price', with: @item.price
+    fill_in 'IVA', with: @item.iva
+    fill_in 'IRPF', with: @item.irpf
+    fill_in 'Item name', with: @item.item_name
+    click_on 'Update Item'
 
-    assert_text "Item was successfully updated"
-    click_on "Back"
+    assert_text 'Item was successfully updated'
+    click_on 'Back'
   end
 
-  test "should destroy Item" do
+  test 'should destroy Item' do
     visit items_path
     assert_text @item.item_name
 
-    click_on "Destroy this item", match: :first
+    accept_alert '¿Estás seguro?' do
+      click_on 'Borrar', match: :first
+    end
 
-    assert_text "Item was successfully destroyed"
+    assert_text 'Ítem eliminado con éxito'
   end
 end
